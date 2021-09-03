14568faf6c7f10fe940c92597ed834367ac9946e
Артем Шевелев <artemsevelev@Artems-MacBook-Pro.local>
2021-09-03 20:30:25 +0300

    add animation and modal

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ class App extends React.Component {
     return (
       <div className="grid-container">
         <header>
-          <a href="/">React Shopping Cart</a>
+          <a href="/">Whist Shopping Cart</a>
         </header>
         <main>
           <div className="content">
@@ -116,7 +116,7 @@ class App extends React.Component {
             </div>
           </div>
         </main>
-        <footer>All right is reserverd.</footer>
+        <footer>All right is reserved.</footer>
       </div>
     );
   }
